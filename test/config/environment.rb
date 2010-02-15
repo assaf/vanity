949e4331b7bd405ad577ac7421eaac0d60c41daf
Assaf Arkin <assaf@labnotes.org>
2010-02-14 22:35:27 -0800

    Started using Gemfile to setup development environment (requires Bundler 0.9.4 or later) Started using autotest
    $ git clone git://github.com/assaf/vanity.git
    $ cd vanity
    $ bundle install
    $ bundle exec autotest -f

--- test/config/environment.rb
+++ test/config/environment.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  config.action_controller.session = { :key=>"_myapp_session", :secret=>"Insert your super awesome one of a kind secret" }
+  config.action_controller.session = { :key=>"_myapp_session", :secret=>"Stay hungry. Stay foolish. -- Steve Jobs" }
   config.after_initialize do
     $:.unshift File.dirname(__FILE__) + "/../../lib/"
     require "vanity"
