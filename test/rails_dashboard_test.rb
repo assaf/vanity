2389a5f926d7319ce0d24f2d33e043b949310ded
Doug Cole <doug@estately.com>
2011-04-22 11:16:34 -0700

    add error checking to add_participant

--- test/rails_dashboard_test.rb
+++ test/rails_dashboard_test.rb
@@ -22,6 +22,12 @@ class RailsDashboardTest < ActionController::TestCase
     assert @response.body.blank?
   end
 
+  def test_add_participant_no_params
+    xhr :post, :add_participant
+    assert_response :not_found
+    assert @response.body.blank?
+  end
+
   def test_chooses
     xhr :post, :chooses, :e => "food", :a => 0
     assert_response :success
