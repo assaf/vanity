ae9e2b03e3dbccb61afa63fbb6f29458a81e9f8c
Anthony Eden <anthonyeden@gmail.com>
2011-05-06 11:52:58 +0200

    only execute the test if Redis is defined.

--- test/adapters/redis_adapter_test.rb
+++ test/adapters/redis_adapter_test.rb
@@ -2,14 +2,16 @@ require 'test/test_helper'
 
 class RedisAdapterTest < Test::Unit::TestCase
   def test_warn_on_disconnect_error
-    assert_nothing_raised do
-      Redis.any_instance.stubs(:connect!)
-      mocked_redis = stub("Redis")
-      mocked_redis.expects(:quit).raises(RuntimeError)
-      redis_adapter = Vanity::Adapters::RedisAdapter.new({})
-      redis_adapter.expects(:warn).with("Error while disconnecting from redis: RuntimeError")
-      redis_adapter.stubs(:redis).returns(mocked_redis)
-      redis_adapter.disconnect!
+    if defined?(Redis)
+      assert_nothing_raised do
+        Redis.any_instance.stubs(:connect!)
+        mocked_redis = stub("Redis")
+        mocked_redis.expects(:quit).raises(RuntimeError)
+        redis_adapter = Vanity::Adapters::RedisAdapter.new({})
+        redis_adapter.expects(:warn).with("Error while disconnecting from redis: RuntimeError")
+        redis_adapter.stubs(:redis).returns(mocked_redis)
+        redis_adapter.disconnect!
+      end
     end
   end
 end
