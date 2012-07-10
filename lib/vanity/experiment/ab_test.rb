de9b36ea6ad3c4f21552b07fd73129913e01d157
Thomas Lotze <thomas@grockit.com>
2012-07-10 16:15:30 -0700

    changed to use new ab_seen function rather than calling ab_show to determine if the participant has seen something

--- lib/vanity/experiment/ab_test.rb
+++ lib/vanity/experiment/ab_test.rb
@@ -127,7 +127,6 @@ module Vanity
         @metrics
       end
 
-
       # -- Alternatives --
 
       # Call this method once to set alternative values for this experiment
@@ -201,13 +200,14 @@ module Vanity
             index = connection.ab_showing(@id, identity)
             unless index
               index = alternative_for(identity)
-              # if we have an on_assignment block, make sure we both store the assignment and call the on_assignment block
-              if @on_assignment_block
-                assignment = alternatives[index.to_i]
-                connection.ab_show(@id, identity, index.to_i)
-                @on_assignment_block.call(Vanity.context, identity, assignment, self)
-              end
               if !@playground.using_js?
+                # if we have an on_assignment block, call it on new assignments
+                if @on_assignment_block
+                  assignment = alternatives[index.to_i]
+                  if !connection.ab_seen @id, identity, assignment
+                    @on_assignment_block.call(Vanity.context, identity, assignment, self)
+                  end
+                end
                 connection.ab_add_participant @id, index, identity
                 check_completion!
               end
