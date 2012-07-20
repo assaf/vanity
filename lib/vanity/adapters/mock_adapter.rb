ac7536aaab6e631428e083599e8c8a4987fee1ad
hellochar <xiaohan@verticalbrands.com>
2012-07-20 11:30:28 -0700

    Mock adapter implementation of new connection methods

--- lib/vanity/adapters/mock_adapter.rb
+++ lib/vanity/adapters/mock_adapter.rb
@@ -69,6 +69,15 @@ module Vanity
       
 
       # -- Experiments --
+
+      def set_experiment_enabled(experiment, enabled)
+        @experiments[experiment] ||= {}
+        @experiments[experiment][:enabled] = enabled
+      end
+
+      def is_experiment_enabled?(experiment)
+        @experiments[experiment] && @experiments[experiment][:enabled]
+      end
      
       def set_experiment_created_at(experiment, time)
         @experiments[experiment] ||= {}
