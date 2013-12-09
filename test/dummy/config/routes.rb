c68a64abd36f4c0bd040cb8c68a83f6897518376
Phill Baker <phillbaker@retrodict.com>
2013-12-08 22:56:16 -0500

    Add POST to routes for dummy rails app.
    * Controller tests don't test routing, just the action name. A separate routing spec/test would be needed for the HTTP verb checking.
    * Closes #168.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -54,5 +54,5 @@ Dummy::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))', :via => [:get]
+  match ':controller(/:action(/:id(.:format)))', :via => [:get, :post]
 end
