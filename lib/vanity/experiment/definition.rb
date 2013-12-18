47d94d484bf9649d87a5e029e1d611c999555671
Phill Baker <phillbaker@retrodict.com>
2013-12-17 22:36:29 -0500

    Fix whitespace and spelling error in Definition.

--- lib/vanity/experiment/definition.rb
+++ lib/vanity/experiment/definition.rb
@@ -2,7 +2,7 @@ module Vanity
   module Experiment
     # These methods are available from experiment definitions (files located in
     # the experiments directory, automatically loaded by Vanity).  Use these
-    # methods to define you experiments, for example:
+    # methods to define your experiments, for example:
     #   ab_test "New Banner" do
     #     alternatives :red, :green, :blue
     #     metrics :signup
@@ -14,17 +14,17 @@ module Vanity
       # Defines a new experiment, given the experiment's name, type and
       # definition block.
       def define(name, type, options = nil, &block)
-      	fail "Experiment #{@experiment_id} already defined in playground" if playground.experiments[@experiment_id]
-      	klass = Experiment.const_get(type.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase })
-      	experiment = klass.new(playground, @experiment_id, name, options)
-      	experiment.instance_eval &block
-      	experiment.save
-      	playground.experiments[@experiment_id] = experiment
+        fail "Experiment #{@experiment_id} already defined in playground" if playground.experiments[@experiment_id]
+        klass = Experiment.const_get(type.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase })
+        experiment = klass.new(playground, @experiment_id, name, options)
+        experiment.instance_eval &block
+        experiment.save
+        playground.experiments[@experiment_id] = experiment
       end
 
       def new_binding(playground, id)
-      	@playground, @experiment_id = playground, id
-      	binding
+        @playground, @experiment_id = playground, id
+        binding
       end
 
     end
