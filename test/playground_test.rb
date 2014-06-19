737d0bf62217967def1e59f6ccf4b99a7a244918
Phill Baker <phillbaker@retrodict.com>
2014-06-18 22:54:29 -0400

    Switch Playground tests over to spec format.

--- test/playground_test.rb
+++ test/playground_test.rb
@@ -1,98 +1,114 @@
 require "test_helper"
 
-class PlaygroundTest < Test::Unit::TestCase
+describe Vanity::Playground do
 
-  def test_has_one_global_instance
+  it "has one global instance" do
     assert instance = Vanity.playground
     assert_equal instance, Vanity.playground
   end
 
-  def test_be_use_js
-    assert !Vanity.playground.using_js?
-    Vanity.playground.use_js!
-    assert Vanity.playground.using_js?
-  end
-
-  def test_be_failover_on_datastore_error
-    assert !Vanity.playground.failover_on_datastore_error?
-    Vanity.playground.failover_on_datastore_error!
-    assert Vanity.playground.failover_on_datastore_error?
-  end
-
-  def test_default_failover_on_datastore_error
-    proc = Vanity.playground.on_datastore_error
-    assert proc.respond_to?(:call)
-    assert_nothing_raised do
-      proc.call(Exception.new("datastore error"), self.class, caller[0][/`.*'/][1..-2], [1, 2, 3])
+  describe "#use_js!" do
+    it "sets via use_js" do
+      assert !Vanity.playground.using_js?
+      Vanity.playground.use_js!
+      assert Vanity.playground.using_js?
     end
   end
 
-  def test_request_filter
-    proc = Vanity.playground.request_filter
-    assert proc.respond_to?(:call)
-    assert_nothing_raised do
-      proc.call(dummy_request)
+  describe "#failover_on_datastore_error" do
+    it "sets failover_on_datastore_error" do
+      assert !Vanity.playground.failover_on_datastore_error?
+      Vanity.playground.failover_on_datastore_error!
+      assert Vanity.playground.failover_on_datastore_error?
     end
   end
 
-  def test_chooses_path_sets_default
-    assert_equal Vanity.playground.add_participant_path, Vanity::Playground::DEFAULT_ADD_PARTICIPANT_PATH
+  describe "#on_datastore_error" do
+    it "has a default failover_on_datastore_error" do
+      proc = Vanity.playground.on_datastore_error
+      assert proc.respond_to?(:call)
+      assert_silent do
+        proc.call(Exception.new("datastore error"), self.class, caller[0][/`.*'/][1..-2], [1, 2, 3])
+      end
+    end
   end
 
-  def test_reconnects_with_existing_connection
-    Vanity.playground.establish_connection "mock:/"
-    Vanity.playground.reconnect!
-    assert_equal Vanity.playground.connection.to_s, "mock:/"
+  describe "#request_filter" do
+    it "sets request_filter" do
+      proc = Vanity.playground.request_filter
+      assert proc.respond_to?(:call)
+      assert_silent do
+        proc.call(dummy_request)
+      end
+    end
   end
 
-  def test_autoconnect_establishes_connection_by_default_with_connection
-    instance = Vanity::Playground.new(:connection=>"mock:/")
-    assert instance.connected?
+  describe "#add_participant_path" do
+    it "sets a default add participant path" do
+      assert_equal Vanity.playground.add_participant_path, Vanity::Playground::DEFAULT_ADD_PARTICIPANT_PATH
+    end
   end
 
-  def test_autoconnect_establishes_connection_by_default
-    Vanity::Playground.any_instance.expects(:establish_connection)
-    Vanity::Playground.new
-  end
-
-  def test_autoconnect_can_skip_connection
-    Vanity::Autoconnect.stubs(:playground_should_autoconnect?).returns(false)
-    instance = Vanity::Playground.new(:connection=>"mock:/")
-    assert !instance.connected?
-  end
-
-  def test_experiments_persisted_returns_true
-    metric "Coolness"
-    new_ab_test :foobar do
-      alternatives "foo", "bar"
-      identify { "abcdef" }
-      metrics :coolness
+  describe "autoconnect" do
+    it "reconnects with existing connection" do
+      Vanity.playground.establish_connection "mock:/"
+      Vanity.playground.reconnect!
+      assert_equal Vanity.playground.connection.to_s, "mock:/"
     end
 
-    assert Vanity.playground.experiments_persisted?
-  end
-
-  def test_experiments_persisted_finds_returns_false
-    name = 'Price'
-    id = :price
-    experiment = Vanity::Experiment::AbTest.new(Vanity.playground, id, name)
-    Vanity.playground.experiments[id] = experiment
-
-    assert !Vanity.playground.experiments_persisted?
-
-    Vanity.playground.experiments.delete(id)
-  end
-
-  def test_participant_info
-    assert_equal [], Vanity.playground.participant_info("abcdef")
-    metric "Coolness"
-    new_ab_test :foobar do
-      alternatives "foo", "bar"
-      identify { "abcdef" }
-      metrics :coolness
+    it "establishes connection by default with connection" do
+      instance = Vanity::Playground.new(:connection=>"mock:/")
+      assert instance.connected?
     end
-    alt = experiment(:foobar).choose
-    assert_equal [[Vanity.playground.experiment(:foobar), alt]], Vanity.playground.participant_info("abcdef")
+
+    it "establishes connection by default" do
+      Vanity::Playground.any_instance.expects(:establish_connection)
+      Vanity::Playground.new
+    end
+
+    it "can skip connection" do
+      Vanity::Autoconnect.stubs(:playground_should_autoconnect?).returns(false)
+      instance = Vanity::Playground.new(:connection=>"mock:/")
+      assert !instance.connected?
+    end
+  end
+
+  describe "experiments_persisted?" do
+    it "returns true" do
+      metric "Coolness"
+      new_ab_test :foobar do
+        alternatives "foo", "bar"
+        identify { "abcdef" }
+        metrics :coolness
+      end
+
+      assert Vanity.playground.experiments_persisted?
+    end
+
+    it "returns false" do
+      name = 'Price'
+      id = :price
+      experiment = Vanity::Experiment::AbTest.new(Vanity.playground, id, name)
+      Vanity.playground.experiments[id] = experiment
+
+      assert !Vanity.playground.experiments_persisted?
+
+      Vanity.playground.experiments.delete(id)
+    end
+  end
+
+  describe "participant_info" do
+    it "returns participant's experiments" do
+      assert_equal [], Vanity.playground.participant_info("abcdef")
+      metric "Coolness"
+      new_ab_test :foobar do
+        alternatives "foo", "bar"
+        identify { "abcdef" }
+        metrics :coolness
+      end
+      alt = experiment(:foobar).choose
+      assert_equal [[Vanity.playground.experiment(:foobar), alt]], Vanity.playground.participant_info("abcdef")
+    end
   end
 
 end
