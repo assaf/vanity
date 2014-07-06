a89cb6d60d5f7a069546b01b8470255f07199551
Phill Baker <phillbaker@retrodict.com>
2014-07-06 15:14:20 -0400

    Remove deprecated action mailer testing for Rails < 3.

--- test/frameworks/rails/action_mailer_test.rb
+++ test/frameworks/rails/action_mailer_test.rb
@@ -8,23 +8,14 @@ class VanityMailer < ActionMailer::Base
   def ab_test_subject(user)
     use_vanity_mailer user
 
-    if defined?(Rails::Railtie)
-      mail :subject =>ab_test(:pie_or_cake).to_s, :body => ""
-    else
-      subject ab_test(:pie_or_cake).to_s
-      body ""
-    end
+    mail :subject =>ab_test(:pie_or_cake).to_s, :body => ""
   end
 
   def ab_test_content(user)
     use_vanity_mailer user
 
-    if defined?(Rails::Railtie)
-      mail do |format|
-        format.html { render :text=>view_context.vanity_tracking_image(Vanity.context.vanity_identity, :open, :host => "127.0.0.1:3000") }
-      end
-    else
-      body vanity_tracking_image(Vanity.context.vanity_identity, :open, :host => "127.0.0.1:3000")
+    mail do |format|
+      format.html { render :text=>view_context.vanity_tracking_image(Vanity.context.vanity_identity, :open, :host => "127.0.0.1:3000") }
     end
   end
 end
