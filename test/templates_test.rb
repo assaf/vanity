e975a49f2b81c1137745e84619c237f8d69535f0
Phill Baker <phillbaker@retrodict.com>
2015-01-13 23:35:29 -0500

    Don't memoize in Vanity::Templates, clean up tests.

--- test/templates_test.rb
+++ test/templates_test.rb
@@ -3,24 +3,26 @@ require "test_helper"
 describe Vanity::Templates do
   describe "template" do
     it "resolves templates from the gem by default" do
-      Vanity::Templates.instance_variable_set("@template_directory", nil)
-      custom_view_path = File.expand_path(File.join(Rails.root || '.', 'app', 'views', 'vanity'))
+      custom_view_path = File.expand_path(File.join(Rails.root, 'app', 'views', 'vanity'))
       gem_view_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'vanity', 'templates'))
       expected = File.join(gem_view_path, 'foo.html')
 
-      File.stubs(:exists?).with(custom_view_path).returns(false)
-      File.stubs(:exists?).with(gem_view_path).returns(true)
-
-      assert_equal expected, Vanity.template('foo.html')
+      assert_equal expected, Vanity::Templates.new.path('foo.html')
     end
 
     it "resolves templates from the Rails view directory if it exists" do
-      Vanity::Templates.instance_variable_set("@template_directory", nil)
-      custom_view_path = File.expand_path(File.join(Rails.root || '.', 'app', 'views', 'vanity'))
-      File.stubs(:exists?).with(custom_view_path).returns(true)
+      begin
+        custom_view_path = File.expand_path(File.join(Rails.root, 'app', 'views', 'vanity'))
 
-      expected = File.expand_path(File.join(custom_view_path, 'foo.html'))
-      assert_equal expected, Vanity.template('foo.html')
+        expected = File.expand_path(File.join(custom_view_path, 'foo.html'))
+
+        FileUtils.mkpath(custom_view_path)
+        File.open(expected, "w")
+
+        assert_equal expected, Vanity::Templates.new.path('foo.html')
+      ensure
+        FileUtils.rm_rf(custom_view_path)
+      end
     end
   end
 end
