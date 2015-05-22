57270d3a2461ba25a1d05ca43e37a271fee51fe2
Phill Baker <phillbaker@retrodict.com>
2015-05-21 21:56:09 -0400

    Clarify tests around track! helper in non-controller contexts.

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -8,9 +8,19 @@ describe Object do
         alternatives "foo", "bar"
         metrics :coolness
       end
-      track! :coolness, :identity=>'quux', :values=>[2]
+      track!(:coolness, :identity=>'quux')
 
-      # experiment(:foobar).alternatives.sum(&:conversions).must_equal 2
+      assert_equal 1, experiment(:foobar).alternatives.sum(&:conversions)
+    end
+
+    it "accepts value for conversion" do
+      metric "Coolness"
+      new_ab_test :foobar do
+        alternatives "foo", "bar"
+        metrics :coolness
+      end
+      track!(:coolness, :identity=>'quux', :values=>[2])
+
       assert_equal 2, experiment(:foobar).alternatives.sum(&:conversions)
     end
   end
