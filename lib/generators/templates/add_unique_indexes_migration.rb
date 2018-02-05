2ac34b68cbc9f9e597830377866eab4064d25461
Michael Pih <mike.pih@gmail.com>
2018-02-05 10:48:46 -0800

    restore original unique index migration

--- lib/generators/templates/add_unique_indexes_migration.rb
+++ lib/generators/templates/add_unique_indexes_migration.rb
@@ -17,8 +17,11 @@ class AddVanityUniqueIndexes < ActiveRecord::Migration
 
   def change
     with_vanity_connection do
-      remove_index :vanity_participants, :name => "by_experiment_id_and_identity"
-      add_index :vanity_participants, [:experiment_id, :identity], :name => "by_experiment_id_and_alternative", :unique => true
+      remove_index :vanity_experiments, [:experiment_id]
+      add_index :vanity_experiments, [:experiment_id], :unique => true
+
+      remove_index :vanity_conversions, :name => "by_experiment_id_and_alternative", :unique => true
+      add_index :vanity_conversions, [:vanity_experiment_id, :alternative], :name => "by_experiment_id_and_alternative", :unique => true
     end
   end
 
